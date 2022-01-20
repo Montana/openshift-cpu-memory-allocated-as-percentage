b73afdff4b3a15ac1725686d498d34dc29820cc2
Montana Mendy <montana@linux.com>
2022-01-20 11:56:53 -0800

    add sed | awk

--- openshift-allocation.sh
+++ openshift-allocation.sh
@@ -1,4 +1,4 @@
-[montana@host ~]# oc get node --no-headers | while read node stat role age ver; do echo -ne "$node\t"; oc describe node $node | egrep '^  (cpu|memory)' | sed -r 's/\(|\)//g' | while read lbl x p1 y p2; do echo -n \ $p1 $p2 ; done; echo ""; done
+[montana@host ~] # oc get node --no-headers | while read node stat role age ver; do echo -ne "$node\t"; oc describe node $node | egrep '^  (cpu|memory)' | sed -r 's/\(|\)//g' | while read lbl x p1 y p2; do echo -n \ $p1 $p2 ; done; echo ""; done
 master0	 43% 0% 28% 3%
 master1	 40% 0% 23% 3%
 master2	 44% 0% 27% 3%
