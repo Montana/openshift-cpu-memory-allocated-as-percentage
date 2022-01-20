847be37667531492c337abdfddfd1be208b6251f
Montana Mendy <montana@linux.com>
2022-01-20 11:59:31 -0800

    list high usage OpenShift nodes

---
+++ list-high-nodes.sh
@@ -0,0 +1,8 @@
+[montana@host ~] # oc adm top node
+NAME      CPU(cores)   CPU%   MEMORY(bytes)   MEMORY%   
+master0   408m         11%    3377Mi          21%       
+master1   546m         15%    2772Mi          17%       
+master2   448m         12%    3325Mi          21%       
+worker0   238m         6%     2151Mi          13%       
+worker1   257m         7%     2188Mi          14%       
+worker2   128m         3%     1367Mi          11% 
